7610e8b3ddc63da3195caab8d75b6ffbbfea29cb
Tomás <tcsao1@iscte-iul.pt>
2024-11-11 23:29:03 +0000

    Criação da class node

--- src/Node.java
+++ src/Node.java
@@ -25,11 +25,37 @@ public class Node {
         //this.incomingMessages = new LinkedList<>();
     }
     //M1.Inicia o servidor
-    //public void startServer()
+    public void startServer() throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server started on port " + port);
+
+        while (true) {
+            Socket connectionSocket = serverSocket.accept();
+            threadPool.submit(() -> handleNewConnection(connectionSocket));
+        }
+    }
     //M2 - Estabelece conexão com outro node
     //public void connectToNode(String ipAddress, int port) {}
     //M3 - Trata uma nova conexão adicionando o node a lista de nós conectados
     //private void handleNewConnection(Socket connectionSocket) {
+    private void handleNewConnection(Socket connectionSocket) {
+        try (ObjectInputStream in = new ObjectInputStream(connectionSocket.getInputStream())) {
+            // Processa a mensagem de conexão
+            Message message = (Message) in.readObject();
+            if (message instanceof NewConnectionRequest) {
+                // Aceita a conexão e envia uma resposta
+                ObjectOutputStream out = new ObjectOutputStream(connectionSocket.getOutputStream());
+                out.writeObject(new ConnectionAcceptedMessage(nodeName, this.ipAddress, this.port));
+
+                // Adiciona o nó à lista de nós conectados
+                connectedNodes.add(new Node(message.getIpAddress(), message.getPort(), message.getNodeName()));
+                System.out.println("Accepted connection from node: " + message.getIpAddress() + ":" + message.getPort());
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     //public void sendMessage(Node destinationNode, Message message) {
     //public void receiveMessages() {
 }
